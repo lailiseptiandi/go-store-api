4ca87095986d6ca2a52b772267896cf16ca828da
tpid-laili <laili@technopartner.co.id>
2022-09-27 16:32:56 +0700

    added product repository

--- repository/product_repository.go
+++ repository/product_repository.go
@@ -1,1 +1,12 @@
 package repository
+
+import "github.com/lailiseptiandi/golang-toko-online/model"
+
+func (r *repository) GetProduct() ([]model.Product, error) {
+	var product []model.Product
+	err := r.db.Find(&product).Error
+	if err != nil {
+		return product, err
+	}
+	return product, nil
+}
