ad82dc619f55d785860fc350a6a6029f03ca7194
Laili Septiandi <lailiseptiandi2@gmail.com>
2023-09-07 08:32:26 +0700

    [added] router

--- main.go
+++ main.go
@@ -1,44 +1,16 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/gin-gonic/gin"
-	"github.com/lailiseptiandi/golang-toko-online/config"
-	"github.com/lailiseptiandi/golang-toko-online/handler"
-	"github.com/lailiseptiandi/golang-toko-online/middleware"
-	"github.com/lailiseptiandi/golang-toko-online/repository"
-	"github.com/lailiseptiandi/golang-toko-online/service"
+	"github.com/lailiseptiandi/golang-toko-online/routers"
 )
 
 func main() {
-	globalRepository := repository.NewRepository()
 
-	globalService := service.NewService(globalRepository)
-	authService := config.NewService()
-
-	globalHandler := handler.NewHandler(globalService, authService)
-	// categoryHandler := handler.NewHandlerCategory(globalService, authService)
-
-	router := gin.Default()
-
-	router.POST("/register", globalHandler.RegisterUser)
-	router.POST("/login", globalHandler.LoginUser)
-	router.POST("/check-email", globalHandler.CheckEmailAvailable)
-	router.PUT("/users/:id", middleware.AuthMiddleware(authService, globalService), globalHandler.UpdateUser)
-	router.GET("/users/:id", middleware.AuthMiddleware(authService, globalService), globalHandler.GetUserByID)
-
-	// category
-	router.GET("category", globalHandler.GetCategory)
-	router.POST("category", middleware.AuthMiddleware(authService, globalService), globalHandler.CreateCategory)
-	router.GET("category/:id", globalHandler.DetailCategory)
-	router.PUT("category/:id", middleware.AuthMiddleware(authService, globalService), globalHandler.UpdateCategory)
-	router.DELETE("category/:id", middleware.AuthMiddleware(authService, globalService), globalHandler.DeleteGategory)
-
-	// product
-	router.GET("product", globalHandler.GetProduct)
-	router.POST("product", middleware.AuthMiddleware(authService, globalService), globalHandler.CreateProduct)
-	router.GET("product/:id", globalHandler.ProductByID)
-	router.PUT("product/:id", middleware.AuthMiddleware(authService, globalService), globalHandler.UpdateProduct)
-	router.DELETE("product/:id", middleware.AuthMiddleware(authService, globalService), globalHandler.DeleteProduct)
-
-	router.Run()
+	r := gin.Default()
+	routers.InitRouter(r)
+	r.Run(fmt.Sprintf("%s:%s", os.Getenv("HOST"), os.Getenv("PORT")))
 }
---
+++ routers/routers.go
@@ -0,0 +1,43 @@
+package routers
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/lailiseptiandi/golang-toko-online/config"
+	"github.com/lailiseptiandi/golang-toko-online/handler"
+	"github.com/lailiseptiandi/golang-toko-online/middleware"
+	"github.com/lailiseptiandi/golang-toko-online/repository"
+	"github.com/lailiseptiandi/golang-toko-online/service"
+)
+
+func InitRouter(router *gin.Engine) {
+	globalRepository := repository.NewRepository()
+
+	globalService := service.NewService(globalRepository)
+	authService := config.NewService()
+
+	globalHandler := handler.NewHandler(globalService, authService)
+	// categoryHandler := handler.NewHandlerCategory(globalService, authService)
+
+	router.POST("/register", globalHandler.RegisterUser)
+	router.POST("/login", globalHandler.LoginUser)
+	router.POST("/check-email", globalHandler.CheckEmailAvailable)
+	router.PUT("/users/:id", middleware.AuthMiddleware(authService, globalService), globalHandler.UpdateUser)
+	router.GET("/users/:id", middleware.AuthMiddleware(authService, globalService), globalHandler.GetUserByID)
+
+	// category
+	router.GET("category", globalHandler.GetCategory)
+	router.POST("category", middleware.AuthMiddleware(authService, globalService), globalHandler.CreateCategory)
+	router.GET("category/:id", globalHandler.DetailCategory)
+	router.PUT("category/:id", middleware.AuthMiddleware(authService, globalService), globalHandler.UpdateCategory)
+	router.DELETE("category/:id", middleware.AuthMiddleware(authService, globalService), globalHandler.DeleteGategory)
+
+	// product
+	router.GET("product", globalHandler.GetProduct)
+	router.POST("product", middleware.AuthMiddleware(authService, globalService), globalHandler.CreateProduct)
+	router.GET("product/:id", globalHandler.ProductByID)
+	router.PUT("product/:id", middleware.AuthMiddleware(authService, globalService), globalHandler.UpdateProduct)
+	router.DELETE("product/:id", middleware.AuthMiddleware(authService, globalService), globalHandler.DeleteProduct)
+
+	// product image
+	router.GET("image/:id", globalHandler.GetImageByProduct)
+}
