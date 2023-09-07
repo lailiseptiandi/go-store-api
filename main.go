a27fe074c2750f55c5371082f424320244b1234d
Laili Septiandi <lailiseptiandi2@gmail.com>
2023-09-07 09:34:08 +0700

    fix. refactor main

--- main.go
+++ main.go
@@ -5,10 +5,19 @@ import (
 	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/lailiseptiandi/golang-toko-online/config"
 	"github.com/lailiseptiandi/golang-toko-online/routers"
+	"gorm.io/gorm"
+)
+
+var (
+	db *gorm.DB = config.GetConnection()
 )
 
 func main() {
+	config.LoadEnv()
+	config.MigrateDatabase(db)
+	defer config.DisconnectDB(db)
 
 	r := gin.Default()
 	routers.InitRouter(r)
