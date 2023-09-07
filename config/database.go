2add8fdb695e3d35911c6304b5a25cadc15635f4
Laili Septiandi <lailiseptiandi2@gmail.com>
2023-09-07 08:32:09 +0700

    [refactor] config to env

--- config/database.go
+++ config/database.go
@@ -3,15 +3,18 @@ package config
 import (
 	"fmt"
 	"log"
+	"os"
+	"path"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func GetConnection() *gorm.DB {
-	// godotenv.Load(path.Join(os.Getenv("HOME"), "../.env"))
-	// dsn := os.Getenv("DB_USERNAME") + ":" + os.Getenv("DB_PASSWORD") + "@tcp(" + os.Getenv("DB_HOST") + ")/" + os.Getenv("DB_DATABASE") + "?charset=utf8mb4&parseTime=True&loc=Local"
-	dsn := "root:123qwe@tcp(127.0.0.1:3306)/golang-web-toko-online?charset=utf8mb4&parseTime=True&loc=Local"
+	godotenv.Load(path.Join(os.Getenv("HOME"), "../.env"))
+	dsn := os.Getenv("DB_USERNAME") + ":" + os.Getenv("DB_PASSWORD") + "@tcp(" + os.Getenv("DB_HOST") + ")/" + os.Getenv("DB_DATABASE") + "?charset=utf8mb4&parseTime=True&loc=Local"
+	// dsn := "root:123qwe@tcp(127.0.0.1:3306)/golang-web-toko-online?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	fmt.Println(dsn)
 	if err != nil {
